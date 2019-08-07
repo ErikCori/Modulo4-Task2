40fb95ff01cf9723d7260a5c7c0ffd01726f8620
Erik Cori <erik199666hotmail.com>
2019-08-07 12:25:47 -0300

    cambios

--- src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
+++ src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDate;
 import java.util.Date;
+
 
 @SpringBootApplication
 public class SalvoApplication {
@@ -16,35 +15,27 @@ public class SalvoApplication {
 		SpringApplication.run(SalvoApplication.class, args);
 	}
 	@Bean
-	public CommandLineRunner initData(PlayerRepository repository){
+	public CommandLineRunner initData(PlayerRepository repository1, GameRepository repository2, GamePlayerRepository repository3){
 		return (args) -> {
 			//save  a couple of players
-			repository.save(new Player("j.bauer@ctu.gov"));
-			repository.save(new Player("c.obrian@ctu.gov"));
-			repository.save(new Player("t.almeida@ctu.gov"));
-			repository.save(new Player("d.palmer@whitehouse.gov"));
-		};
-	}
-	public CommandLineRunner initData(GameRepository repository){
-		return (args) -> {
-			//save  a couple of players
-			LocalDate dateGame1 = LocalDate.parse("2/17/2018, 3:20:15 PM");
-			LocalDate dateGame2 = LocalDate.parse("2/17/2018, 4:20:15 PM");
-			LocalDate dateGame3 = LocalDate.parse("")
 
-			repository.save(new Game(dateGame1));
-			repository.save(new Game(dateGame2));
-			repository.save(new Game(dateGame3));
-		};
-	}
-	public CommandLineRunner initData(GamePlayerRepository repository){
-		return (args) -> {
-			//save  a couple of players
-			repository.save(new GamePlayer());
-			repository.save(new Player("Juan"));
-			repository.save(new Player("Matias"));
-			repository.save(new Player("Micaela"));
-			repository.save(new Player("Erik"));
+			repository1.save(new Player("j.bauer@ctu.gov"));
+			repository1.save(new Player("c.obrian@ctu.gov"));
+			repository1.save(new Player("t.almeida@ctu.gov"));
+			repository1.save(new Player("d.palmer@whitehouse.gov"));
+
+			//save a couple of games
+
+			Date dateGame1 =  new Date();
+			Date dateGame2 =  new Date();
+			Date dateGame3 =  new Date();
+
+
+			repository2.save(new Game(dateGame1));
+			repository2.save(new Game(dateGame2));
+			repository2.save(new Game(dateGame3));
+
+			//save a couple of PlayerGames
 		};
 	}
 
